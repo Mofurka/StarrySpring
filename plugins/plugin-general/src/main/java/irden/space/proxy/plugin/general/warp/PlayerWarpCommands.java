a32e424b8c1ff85a21577e8c2bed9e9c863e5bc2
Mofuro <alexandr.xabarov@gmail.com>
2026-07-30 10:49:23 +0500

    feat: update warp command handling

--- plugins/plugin-general/src/main/java/irden/space/proxy/plugin/general/warp/PlayerWarpCommands.java
+++ plugins/plugin-general/src/main/java/irden/space/proxy/plugin/general/warp/PlayerWarpCommands.java
@@ -9,7 +9,20 @@ import irden.space.proxy.plugin.player_manager.command.PlayerOnlineTargetArgumen
 import irden.space.proxy.plugin.player_manager.command.PlayerTarget;
 import irden.space.proxy.plugin.player_manager.model.Player;
 import irden.space.proxy.plugin.player_manager.model.player_position.CelestialWorld;
+import irden.space.proxy.plugin.player_manager.model.player_position.InstanceWorld;
+import irden.space.proxy.plugin.player_manager.model.player_position.PlayerShip;
 import irden.space.proxy.protocol.codec.variant.Variants;
+import irden.space.proxy.protocol.packet.PacketType;
+import irden.space.proxy.protocol.payload.common.celestial_coordinates.CelestialCoordinates;
+import irden.space.proxy.protocol.payload.common.vectors.StarVec2F;
+import irden.space.proxy.protocol.payload.common.warp.action.ToWorldWarpAction;
+import irden.space.proxy.protocol.payload.common.warp.action.WarpAction;
+import irden.space.proxy.protocol.payload.common.warp.target.CelestialWorldTarget;
+import irden.space.proxy.protocol.payload.common.warp.target.PlayerWorldTarget;
+import irden.space.proxy.protocol.payload.common.warp.target.UniqueWorldTarget;
+import irden.space.proxy.protocol.payload.common.warp.target.WorldTarget;
+import irden.space.proxy.protocol.payload.packet.warp.consts.SpawnTarget;
+import irden.space.proxy.protocol.payload.packet.warp.player_warp.PlayerWarp;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -40,9 +53,7 @@ public class PlayerWarpCommands {
         if (to != null) {
             warp(ctx, from, to.player());
         } else {
-            ctx.sender(Player.class).ifPresent(player -> {
-                warp(ctx, player, from);
-            });
+            ctx.sender(Player.class).ifPresent(player -> warp(ctx, player, from));
 
         }
 
@@ -58,24 +69,30 @@ public class PlayerWarpCommands {
         var fromLocation = from.position().getCurrentLocation();
         var toLocation = to.position().getCurrentLocation();
 
-        if (fromLocation instanceof CelestialWorld fromCelestial && toLocation instanceof CelestialWorld toCelestial) {
-            if (fromCelestial.equals(toCelestial)) {
+        if (fromLocation instanceof CelestialWorld fromCelestial && toLocation instanceof CelestialWorld toCelestial && fromCelestial.equals(toCelestial)) {
                 entityMessageService.sendToEntity(from.sessionContext(), from.entityId(), "warp", Variants.of(
                         "Nowhere=" + to.position().getX().intValue() + "." + to.position().getY().intValue()
                 ));
                 ctx.reply("Warp has been sent!");
                 return;
             }
-        }
 
-//        ToPlayerWarpAction toPlayerWarpAction = new ToPlayerWarpAction(to.uuid());
-//        PlayerWarp playerWarp = new PlayerWarp(toPlayerWarpAction, false);
-//        ctx.session().sendToServer(PacketType.PLAYER_WARP, playerWarp);
-        entityMessageService.sendToEntity(from.sessionContext(), from.entityId(), "warp", Variants.of(
-                "player:" + to.uuid().toString()
-        ));
+        WorldTarget worldTarget = switch (toLocation) {
+            case CelestialWorld(var location, var planet, var satellite) -> {
+                var cc = new CelestialCoordinates(location, planet, satellite);
+                var ue = new SpawnTarget.Position(new StarVec2F(to.position().getX(), to.position().getY()));
+                yield new CelestialWorldTarget(cc, ue);
+            }
+            case InstanceWorld(var worldName, var instanceUuid) ->
+                    new UniqueWorldTarget(worldName, instanceUuid, null, null);
+            case PlayerShip(var shipUuid) ->
+                    new PlayerWorldTarget(shipUuid, new SpawnTarget.Position(new StarVec2F(to.position().getX(), to.position().getY())));
+        };
+
+        WarpAction warpAction = new ToWorldWarpAction(worldTarget);
+        PlayerWarp playerWarp = new PlayerWarp(warpAction, false);
+        from.sessionContext().sendToServer(PacketType.PLAYER_WARP, playerWarp);
         ctx.reply("Warp has been sent!");
-
     }
 
 }
